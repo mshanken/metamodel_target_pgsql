5223b80ed0f4bc5b68ab51b63afb163ca130b0ad
chandeeland <chand@chandeeland.org>
2013-02-13 14:56:50 -0500

    modifications to make discover/docs and discover/routes work

--- classes/Metamodel/Target/Pgsql.php
+++ classes/Metamodel/Target/Pgsql.php
@@ -69,7 +69,7 @@ implements Target_Selectable
     {   
         $info = $entity->target_pgsql_info();
         $entity['pgsql_mutable']->validate();
-        if (!empty($entity['pgsql_mutable']->problems()))
+        if (!strlen($entity['pgsql_mutable']->problems()))
         {
             throw new HTTP_Exception_400($entity['pgsql_mutable']->problems());
         }
